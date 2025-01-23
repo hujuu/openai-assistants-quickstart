908204103de7582b843071e9947c767b0f5f9655
hujuu <1961946+hujuu@users.noreply.github.com>
2025-01-23 09:26:55 +0900

    Update route.ts

--- app/api/files/[fileId]/route.ts
+++ app/api/files/[fileId]/route.ts
@@ -1,10 +1,13 @@
 import { openai } from "@/app/openai";
 
 // download file by file ID
-export async function GET(_request, { params: { fileId } }) {
+export async function GET(
+    _request: Request,
+    { params }: { params: Promise<{ fileId: string }> }
+) {
   const [file, fileContent] = await Promise.all([
-    openai.files.retrieve(fileId),
-    openai.files.content(fileId),
+    openai.files.retrieve((await params).fileId),
+    openai.files.content((await params).fileId),
   ]);
   return new Response(fileContent.body, {
     headers: {
