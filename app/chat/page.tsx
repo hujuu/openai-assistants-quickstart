672a7fd0a69ccdf2af588e979af72ebda0a57e54
hujuu <1961946+hujuu@users.noreply.github.com>
2025-01-23 18:52:07 +0900

    Update page.tsx

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,24 +1,25 @@
 "use client";
-import React from "react";
+import { useAuth } from "../hooks/useAuth";
 import styles from "../examples/shared/page.module.css";
-
 import Chat from "../components/chat";
 import FileViewer from "../components/file-viewer";
 
 const FileSearchPage = () => {
+  useAuth(); // ログインチェックのみ実行
+
   return (
-    <main className={styles.main}>
-      <div className={styles.container}>
-        <div className={styles.column}>
-          <FileViewer />
-        </div>
-        <div className={styles.chatContainer}>
-          <div className={styles.chat}>
-            <Chat />
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <div className={styles.column}>
+            <FileViewer />
+          </div>
+          <div className={styles.chatContainer}>
+            <div className={styles.chat}>
+              <Chat />
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
   );
 };
 
