417021f4206cf053124520b0928e0d44c2445383
hujuu <1961946+hujuu@users.noreply.github.com>
2025-01-28 00:20:27 +0900

    Update page.tsx

--- app/chat/[chatId]/page.tsx
+++ app/chat/[chatId]/page.tsx
@@ -4,8 +4,10 @@ import React from "react";
 import styles from "./page.module.css";
 import Chat from "@/app/components/chat";
 import ChatStream from "@/app/components/chat-stream";
+import {useAuth} from "@/app/hooks/useAuth";
 
 const Home = () => {
+  useAuth();
   return (
     <main className={styles.main}>
       <div className={styles.container}>
